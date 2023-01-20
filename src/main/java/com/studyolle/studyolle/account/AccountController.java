4faea1fd6890d4a1c9fc80190c7723c9b215b05b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-20 21:31:50 +0900

    회원 가입 폼 서브밋 검증

--- src/main/java/com/studyolle/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/studyolle/account/AccountController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -16,6 +17,11 @@ public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
 
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm());
@@ -24,12 +30,6 @@ public class AccountController {
 
     @PostMapping("/sign-up")
     public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
-        if (errors.hasErrors()) {
-            return "account/sign-up";
-        }
-
-        signUpFormValidator.validate(signUpForm, errors);
-
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
