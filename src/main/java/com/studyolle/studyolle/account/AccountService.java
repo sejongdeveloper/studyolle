01133147ae4c0975ae3fe46e787d32d79355c55a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-29 23:39:58 +0900

    관심 주제 조회

--- src/main/java/com/studyolle/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/studyolle/account/AccountService.java
@@ -21,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -124,4 +125,10 @@ public class AccountService implements UserDetailsService {
         Optional<Account> byId = accountRepository.findById(account.getId());
         byId.ifPresent(a -> a.getTags().add(tag));
     }
+
+    public Set<Tag> getTags(Account account) {
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        return byId.orElseThrow().getTags();
+    }
+
 }
