32dae70acece93f063f9c7e9acbd9c5d3966ff27
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-02-05 00:55:51 +0900

    알림 도메인

---
+++ src/main/java/com/studyolle/studyolle/modules/notification/Notification.java
@@ -0,0 +1,38 @@
+package com.studyolle.studyolle.modules.notification;
+
+import com.studyolle.studyolle.modules.account.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
