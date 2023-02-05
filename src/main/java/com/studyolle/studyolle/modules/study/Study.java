ff0018a1d7fbf7acb1781c02c3fb6ea9723a6f8d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-02-05 13:56:05 +0900

    검색 뷰 개선

--- src/main/java/com/studyolle/studyolle/modules/study/Study.java
+++ src/main/java/com/studyolle/studyolle/modules/study/Study.java
@@ -72,6 +72,8 @@ public class Study {
 
     private boolean useBanner;
 
+    private int memberCount = 0;
+
     public void addManager(Account account) {
         this.managers.add(account);
     }
@@ -145,10 +147,12 @@ public class Study {
 
     public void addMember(Account account) {
         this.getMembers().add(account);
+        this.memberCount++;
     }
 
     public void removeMember(Account account) {
         this.getMembers().remove(account);
+        this.memberCount--;
     }
 
     public String getEncodedPath() {
