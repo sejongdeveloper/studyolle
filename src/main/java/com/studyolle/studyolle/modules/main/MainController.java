d8e6bcdd745efd358af5b4e3b00c7a84f782dd6b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-02-05 12:24:35 +0900

    검색 기능 구현

--- src/main/java/com/studyolle/studyolle/modules/main/MainController.java
+++ src/main/java/com/studyolle/studyolle/modules/main/MainController.java
@@ -2,12 +2,20 @@ package com.studyolle.studyolle.modules.main;
 
 import com.studyolle.studyolle.modules.account.CurrentAccount;
 import com.studyolle.studyolle.modules.account.Account;
+import com.studyolle.studyolle.modules.study.Study;
+import com.studyolle.studyolle.modules.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final StudyRepository studyRepository;
 
     @GetMapping("/")
     public String home(@CurrentAccount Account account, Model model) {
@@ -21,5 +29,13 @@ public class MainController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/search/study")
+    public String searchStudy(String keyword, Model model) {
+        List<Study> studyList = studyRepository.findByKeyword(keyword);
+        model.addAttribute(studyList);
+        model.addAttribute("keyword", keyword);
+        return "search";
     }
 }
